4bb253c17b53b3da1571b35684d1cf194cb19cbf
Yatim Mandiri <muhammadhamdani017@gmail.com>
2025-09-08 18:43:09 +0700

    Update list.tsx

--- resources/js/pages/admin/cms/tag/list.tsx
+++ resources/js/pages/admin/cms/tag/list.tsx
@@ -113,7 +113,7 @@ export default function ListPage() {
 
     return (
         <AppLayout>
-            <Head title="Categories List" />
+            <Head title="Tag List" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative flex min-h-[100vh] flex-1 flex-col space-y-4 overflow-hidden rounded-xl border md:min-h-min">
                     <DataTableProvider
